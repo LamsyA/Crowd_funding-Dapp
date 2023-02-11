eff34c0dd631babdf129ac20510f87ddd4940254
elboffin <olamide.adetula.1744@gmail.com>
2023-02-11 20:46:06 +0100

    Global state Modal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { setGlobalState } from "../store"
 
 
 const Hero = () => {
@@ -13,7 +14,9 @@ const Hero = () => {
    <button type='button'
             className='inline-block bg-lime-500 px-5 py-2 text-white
             font-medium text-xs leading-tight uppercase rounded-full 
-            shadow-md hover:bg-lime-600'> 
+            shadow-md hover:bg-lime-600'
+            onClick={()=> setGlobalState("createModal","scale-100")}
+            > 
              Add Project
     </button>
 
