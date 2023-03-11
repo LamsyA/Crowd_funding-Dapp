0674b3dc193394c3d4ba9950d9a03eb1e96aa586
lamsyA <olamide.adetula.1744@gmail.com>
2023-03-11 22:10:46 +0100

    Update Project

--- src/components/UpdateProject.jsx
+++ src/components/UpdateProject.jsx
@@ -1,21 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { toast } from 'react-toastify'
+import { createNewProject } from '../services/blockchain'
 import { useGlobalState, setGlobalState } from '../store'
 
 const UpdateProject = () => {
     const [updateModal] = useGlobalState('updateModal')
+    const [title, setTitle] = useState(project?.title)
+    const [description, setDescription] = useState(project?.description)
+    const [cost, setCost] = useState(project?.cost)
+    const [date, setDate] = useState(project?.date)
+    const [imageURL, setImageURL] = useState(project?.imageURL)
+
+    const toTimestamp =(dateData) => {
+        const dateObj = Date.parse(dateData)
+        return dateObj / 1000 
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if(!title || !description || !cost || !date || !imageURL) return
+
+    
+    const data = { title,
+            description,
+            imageURL,
+            cost,
+            expiresAt: toTimestamp(date)
+        }
+            console.log(data)
+
+        // await createNewProject(data)
+        toast.success('Project created successfully')
+        
+        onClose()
+    }
+    const onClose = () => {
+        setGlobalState("updateModal","scale-0")
+        resetParam()
+    }
+     const resetParam = () => {
+        setTitle('')
+        setCost('')
+        setDescription('')
+        setImageURL('')
+        setDate('')
+     }
+
+    
+
   return (
     <div className={`fixed top-0 left-0 w-screen h-screen flex
     items-center justify-center bg-black bg-opacity-50
     transform transition-transform duration-300 ${updateModal}`}>
     <div className='bg-white shadow-xl shadow-black rounded-xl
     w-11/12 md:w-2/5 h-7/12 p-6'>
-        <form className='flex flex-col'> 
+        <form onSubmit={handleSubmit} className='flex flex-col'> 
             <div className='flex justify-between items-center'>
-                <p className='font-semibold '>Edit Project</p>
+                <p className='font-semibold '>Add Project</p>
             <button type='button'
             className='border-0 bg-transparent focus:outline-none '
-            onClick={()=> setGlobalState("updateModal","scale-0")}
+            onClick={onClose}
             
             >
                 <FaTimes/>
@@ -23,7 +69,7 @@ const UpdateProject = () => {
             </div>
             <div  className='flex justify-center items-center mt-5'>
                 <div className='rounded-xl overflow-hidden h-20 w-20'>
-                <img src="https://media.wired.com/photos/5926e64caf95806129f50fde/master/pass/AnkiHP.jpg"
+                <img src={imageURL || "https://media.wired.com/photos/5926e64caf95806129f50fde/master/pass/AnkiHP.jpg"}
                     alt="project title" 
                     className=" h-full w-full cursor-pointer object-cover    "
                     />
@@ -38,6 +84,8 @@ const UpdateProject = () => {
                  type='text'
                  name="title"
                  placeholder='Title'
+                 onChange={(e)=> setTitle(e.target.value)}
+                 value={title}
                  required />
             </div>
             <div className='flex justify-between items-center bg-gray-300
@@ -49,8 +97,10 @@ const UpdateProject = () => {
                  type='number'
                  step={0.01}
                  min={0.01}
-                 name="amount"
-                 placeholder='Amount {ETH}'
+                 name="cost"
+                 placeholder='cost {ETH}'
+                 onChange={(e)=> setCost(e.target.value)}
+                 value={cost}
                  required />
             </div>
             <div className='flex justify-between items-center bg-gray-300
@@ -62,6 +112,8 @@ const UpdateProject = () => {
                  type='date'
                  name="date"
                  placeholder='Expiration date'
+                 onChange={(e)=> setDate(e.target.value)}
+                 value={date}
                  required />
             </div>
             <div className='flex justify-between items-center bg-gray-300
@@ -73,6 +125,8 @@ const UpdateProject = () => {
                  type='url'
                  name="imageURL"
                  placeholder='Image URL'
+                 onChange={(e)=> setImageURL(e.target.value)}
+                 value={imageURL}
                  required />
             </div>
             <div className='flex justify-between items-center bg-gray-300
@@ -84,13 +138,15 @@ const UpdateProject = () => {
                  type='text'
                  name="description"
                  placeholder='Description'
+                 onChange={(e)=> setDescription(e.target.value)}
+                 value={description}
                  required ></textarea>
             </div>
             <button type='submit' 
             className='inline-block bg-lime-600 px-6 py-2.5 text-white
             font-medium  leading-tight text-md rounded-full 
-            shadow-md hover:bg-lime-700 mt-5 '> 
-             Update Project 
+            shadow-md hover:bg-lime-700 mt-5'> 
+             Submit Project
             </button>
         </form>
     </div>
